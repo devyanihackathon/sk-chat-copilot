7d44636e5ce3060bc84424cae1c0c1c2e81ba1af
Shan Thayyullathil <shthay@microsoft.com>
2023-11-30 10:48:27 -0600

    Updated the PTI Functions

--- webapi/Plugins/NativePlugins/PTILabelLookup.cs
+++ webapi/Plugins/NativePlugins/PTILabelLookup.cs
@@ -9,18 +9,17 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using System;
 using Microsoft.Data.SqlClient;
-
+using DocumentFormat.OpenXml.Wordprocessing;
 
 namespace CopilotChat.WebApi.Plugins.NativePlugins;
 
 public class PTILabelLookup
 {
-
   [SKFunction, Description("Validate PTI Image")]
   public async Task<string> ValidatePTIImage(string filename)
   {
     string responseContent = string.Empty;
-    string filePath = "C:\\Images\\" + filename;
+    string filePath = ("C:\\temp\\sams\\pti\\" + filename);
     string url = "";
 
     try
@@ -30,7 +29,7 @@ public class PTILabelLookup
         using (var content = new MultipartFormDataContent())
         {
           byte[] fileBytes = File.ReadAllBytes(filePath);
-          content.Add(new ByteArrayContent(fileBytes, 0, fileBytes.Length), "file", "filename");
+          content.Add(new ByteArrayContent(fileBytes, 0, fileBytes.Length), "file", filename);
 
           var response = await client.PostAsync(url, content);
 
@@ -47,34 +46,48 @@ public class PTILabelLookup
     }
     catch (Exception ex)
     {
-      throw new Exception("An error occurred: " + ex.Message);
+      Console.WriteLine(ex.Message);
+      throw new Exception("An error occurred: " + ex.Message);     
     }
     return responseContent;
   }
-
+  
 
   [SKFunction, Description("Get Product Details using PTI Number")]
   public async Task<string> GetProductDetails(string ptinumber)
   {
-    string connectionString = "Server=tcp:your_server.database.windows.net,1433;Initial Catalog=your_database;Persist Security Info=False;User ID=your_username;Password=your_password;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+    //string connectionString = "Server=tcp:your_server.database.windows.net,1433;Initial Catalog=your_database;Persist Security Info=False;User ID=your_username;Password=your_password;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+    string connectionString = Environment.GetEnvironmentVariable("PRODUCT_DETAIL_DB");
 
     using (SqlConnection connection = new SqlConnection(connectionString))
     {
       connection.Open();
+      string dbResponse = "";
 
-      using (SqlCommand command = new SqlCommand("SELECT * FROM your_table", connection))
+      string sqlQuery = "SELECT PTI_NUM,ITEM_NBR,PRODUCT_NM, SUPPLIER_NAME,ROUTE_NO,TRAILER_NO,SEAL_NO,QUALITY_TIPS," +  
+                        "ACCEPTABLE_IMAGE, UNACCEPTABLE_IMAGE_1, UNACCEPTABLE_IMAGE_2,UNACCEPTABLE_IMAGE_3, " +
+                        "QUALITY_ISSUE_1, QUALITY_ISSUE_2, QUALITY_ISSUE_3 " +
+                        " FROM PRODUCT_DETAILS WHERE PTI_NUM = '" + ptinumber + "'";
+
+      using (SqlCommand command = new SqlCommand(sqlQuery, connection))
       {
         using (SqlDataReader reader = command.ExecuteReader())
         {
           while (reader.Read())
           {
-            Console.WriteLine(String.Format("{0}, {1}", reader[0], reader[1]));
+              dbResponse = dbResponse + "    " + String.Format("{0}, {1}", reader[0], reader[1]); 
           }
+          Console.WriteLine(dbResponse);
         }
       }
-      return "";
+      return "dbResponse";
     }
   }
 
+  static string EncodeImageToBase64(string imagePath)
+  {
+    byte[] imageBytes = File.ReadAllBytes(imagePath);
+    return Convert.ToBase64String(imageBytes);
+  }
 }
-
